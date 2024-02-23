816591971195216a258787f8633c8e1f1cfc9dcb
Caitex212 <firewizard787@gmail.com>
2024-02-23 15:32:25 +0100

    after death nothing happens

--- Player.java
+++ Player.java
@@ -14,69 +14,73 @@ public class Player extends Actor
     
     public boolean right = false;
     public boolean left = false;
+    
+    public boolean dead = false;
  
     public void act() {
-        if (vel > maxVel) {
-            vel = maxVel;
-        }
-        if (vel >= 0) {
-            if (isTouching(Floor.class) || isTouching(Platform.class) || isTouching(Platform2.class)) {
-                vel = -20;
-                Greenfoot.playSound("boing.mp3");  
+        if(!dead) {
+            if (vel > maxVel) {
+                vel = maxVel;
             }
-        }
-        
-        if ((vel < 0 && getY() >= 400) || getY() >= 400) {
-            setLocation(getX(), getY() + vel);
-        } else {
-            setLocation(getX(), 400);
-            if(vel < 0) {
-                ((viewWorld) getWorld()).offset = vel*-1;
-                ((viewWorld) getWorld()).showText("Score: " + score, 100, 780);
-                score += vel*(-1);
+            if (vel >= 0) {
+                if (isTouching(Floor.class) || isTouching(Platform.class) || isTouching(Platform2.class)) {
+                    vel = -20;
+                    Greenfoot.playSound("boing.mp3");  
+                }
             }
-        }
-        vel += gravity;
-        
-        if (Greenfoot.isKeyDown("LEFT")) {
-            setImage("wombatleft.png");
-            if(velX > 0) {
+            
+            if ((vel < 0 && getY() >= 400) || getY() >= 400) {
+                setLocation(getX(), getY() + vel);
+            } else {
+                setLocation(getX(), 400);
+                if(vel < 0) {
+                    ((viewWorld) getWorld()).offset = vel*-1;
+                    ((viewWorld) getWorld()).showText("Score: " + score, 100, 780);
+                    score += vel*(-1);
+                }
+            }
+            vel += gravity;
+            
+            if (Greenfoot.isKeyDown("LEFT")) {
+                setImage("wombatleft.png");
+                if(velX > 0) {
+                    velX = 0;
+                }
+                velX -= speed;
+                left = true;
+                if (velX <= -maxVelX) {
+                    velX = -maxVelX;
+                }
+                if(getX() <= 1) {
+                    setLocation(500, getY());
+                }
+            }else {
+                    left = false;
+            }
+            if (Greenfoot.isKeyDown("RIGHT")) {
+                setImage("wombat.png");
+                if(velX < 0) {
+                    velX = 0;
+                }
+                velX += speed;
+                right = true;
+                if (velX >= maxVelX) {
+                    velX = maxVelX;
+                }
+                if(getX() >= 499) {
+                    setLocation(0, getY());
+                }
+            }else {
+                    right = false;
+            }
+            if(!right && !left) {
                 velX = 0;
             }
-            velX -= speed;
-            left = true;
-            if (velX <= -maxVelX) {
-                velX = -maxVelX;
+            if(getY() >= 799) {
+                dead = true;
+                ((viewWorld) getWorld()).generateDeath();
             }
-            if(getX() <= 1) {
-                setLocation(500, getY());
-            }
-        }else {
-                left = false;
+            setLocation(getX() + velX, getY());
         }
-        if (Greenfoot.isKeyDown("RIGHT")) {
-            setImage("wombat.png");
-            if(velX < 0) {
-                velX = 0;
-            }
-            velX += speed;
-            right = true;
-            if (velX >= maxVelX) {
-                velX = maxVelX;
-            }
-            if(getX() >= 499) {
-                setLocation(0, getY());
-            }
-        }else {
-                right = false;
-        }
-        if(!right && !left) {
-            velX = 0;
-        }
-        if(getY() >= 799) {
-            ((viewWorld) getWorld()).generateDeath();
-        }
-        setLocation(getX() + velX, getY());
     }
-
 }
